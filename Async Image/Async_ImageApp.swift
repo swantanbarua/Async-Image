59b3e95286d772b1b1ca3a0aec15c46682cd7604
Swantan Barua <work.swantan@gmail.com>
2024-01-25 19:40:25 +0530

    Initial Commit

---
+++ Async Image/Async_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_ImageApp.swift
+//  Async Image
+//
+//  Created by Swantan Barua on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Async_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
