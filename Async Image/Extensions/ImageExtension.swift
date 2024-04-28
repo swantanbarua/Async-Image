25bdf51705a23b56666f12a1b2f1df617737c7c4
Swantan Barua <work.swantan@gmail.com>
2024-04-28 17:57:14 +0530

    Write comments for extension functions

--- Async Image/Extensions/ImageExtension.swift
+++ Async Image/Extensions/ImageExtension.swift
@@ -8,18 +8,22 @@
 import SwiftUI
 
 extension Image {
-    
+    // Apply modifiers to make the image resizable and scaled to fit its frame.
     func imageModifier() -> some View {
         self
             .resizable()
             .scaledToFit()
     }
     
+    // Apply additional modifiers to the image:
+    // - Set a maximum width of 128 pixels.
+    // - Change foreground color to purple.
+    // - Adjust opacity to 0.5.
     func iconModifier() -> some View {
         self
-            .imageModifier()
-            .frame(maxWidth: 128)
-            .foregroundStyle(Color.purple)
-            .opacity(0.5)
+            .imageModifier() // Apply the basic image modifiers
+            .frame(maxWidth: 128) // Set maximum width of the image
+            .foregroundStyle(Color.purple) // Set foreground color to purple
+            .opacity(0.5) // Set opacity to 0.5
     }
 }
