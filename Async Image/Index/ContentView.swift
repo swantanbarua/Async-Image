5193f5c8515bb4eea3e7d4122935516338b997e8
Swantan Barua <work.swantan@gmail.com>
2024-04-28 17:58:35 +0530

    Write comments for loading remote images

--- Async Image/Index/ContentView.swift
+++ Async Image/Index/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: - PROPERTIES
+    
+    // The URL string for the image to be displayed asynchronously.
     private let imageURLString = "https://credo.academy/.credo-academy@3x.png"
     
     // MARK: - BODY
+    
     var body: some View {
+        // Asynchronously load an image from the provided URL, with a specified scale and transaction.
         AsyncImage(
             url: URL(string: imageURLString),
             scale: 3.0,
@@ -23,27 +27,31 @@ struct ContentView: View {
                 blendDuration: 0.6
             ))
         ) { phase in
-          
+            // Switch over the loading phase of the image.
             switch phase {
                 
             case .success(let image):
-                image
-                    .imageModifier()
+                // If the image is successfully loaded, apply custom modifiers using `imageModifier()`.
+                image.imageModifier()
                 
             case .empty:
+                // If the image is empty, display a default photo icon with custom modifiers.
                 Image(systemName: "photo.circle.fill")
                     .iconModifier()
                 
             case .failure(_):
+                // If there is a failure in loading the image, display a default icon.
                 Image(systemName: "ant.circle.fill")
                 
             default:
+                // Show a progress view while the image is being loaded.
                 ProgressView()
             }
         }
     }
 }
 
+// Preview the ContentView.
 #Preview {
     ContentView()
 }
