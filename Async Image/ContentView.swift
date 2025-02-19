15b261cc33ab47247e2d7e5562ff0034da716902
Swantan Barua <work.swantan@gmail.com>
2025-02-19 13:47:19 +0530

    Remote image access through Async

--- Async Image/ContentView.swift
+++ Async Image/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - PROPERTIES
+    private let imageURLString = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        AsyncImage(
+            url: URL(string: imageURLString),
+            scale: 3.0,
+            transaction: Transaction(
+                animation: .spring(
+                    response: 0.25,
+                    dampingFraction: 0.5,
+                    blendDuration: 0.6
+                )
+            )
+        ) { phase in
+            
+            switch phase {
+            
+            case .success(let image):
+                image
+                    .imageModifier()
+                
+            case .empty:
+                Image(systemName: "photo.circle.fill")
+                    .iconModifier()
+                
+            case .failure(_):
+                Image(systemName: "ant.circle.fill")
+                    .iconModifier()
+                
+            default:
+                ProgressView()
+            }
         }
-        .padding()
     }
 }
 
